4575c3a33aab991adb0c4d6c5e9ce56866069498
Benz1n <benzinych@gmail.com>
2020-11-02 15:27:05 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+
+namespace Stopwatch
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            var timer = new TimeCount();
+
+            while (true)
+            {
+                Console.WriteLine(" 's' to start\n 'f' to finish\n 'q' to quit");
+                var input = Console.ReadLine();
+
+                if (input=="s")
+                {
+                    timer.Start();
+                    Console.WriteLine("Timer has started");
+                }
+                else if (input=="f")
+                {
+                    var result = timer.Stop(DateTime.Now);
+                    Console.WriteLine("Time spend: " + result);
+                    continue;
+                }
+                else if (input=="q")
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Only 's', 'f' and 'q' are available");
+                }
+            }
+
+            
+        }
+    }
+}
---
+++ TimeCount.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Stopwatch
+{
+    partial class Program
+    {
+        public class TimeCount
+        {
+            // private instances of the class (so they can't be modified from outside)
+            // модификатор доступа private, чтобы нельзя было напрямую поменять значения членов класса
+            private DateTime _startTime;
+            private TimeSpan _timer;
+            private bool isWorking = false;
+
+            // methods. методы.
+            public void Start()
+            {
+                if (isWorking)
+                {
+                    throw new InvalidOperationException("You can't start a new timer before previous one is working");
+                }
+                else
+                {
+                    isWorking = true;
+                    _startTime = DateTime.Now;
+                }
+            }
+
+            public TimeSpan Stop(DateTime time)
+            {
+                isWorking = false;
+                _timer = time - _startTime;
+                return _timer;
+            }
+        }
+    }
+}
